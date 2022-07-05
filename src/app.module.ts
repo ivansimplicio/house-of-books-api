e8bcf7f70731147fc02fb11d29b405d0b4d6a239
ivansimplicio <ivansimplicio@outlook.com>
2022-07-05 08:44:25 -0300

    chore: graphql and apollo server configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 import ormConfig from 'ormconfig';
+import { join } from 'path';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(ormConfig as TypeOrmModuleOptions)],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(ormConfig as TypeOrmModuleOptions),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
