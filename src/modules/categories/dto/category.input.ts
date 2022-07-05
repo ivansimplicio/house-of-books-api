cdc67dea25216a1c0597d835a8990c4a0801c63c
ivansimplicio <ivansimplicio@outlook.com>
2022-07-05 17:15:30 -0300

    feat: validator for data entry when inserting and updating a category

--- src/modules/categories/dto/category.input.ts
+++ src/modules/categories/dto/category.input.ts
@@ -1,7 +1,10 @@
 import { InputType, Field } from '@nestjs/graphql';
+import { IsNotEmpty, MaxLength } from 'class-validator';
 
 @InputType()
 export class CategoryInput {
   @Field(() => String)
+  @IsNotEmpty({ message: 'The {name} field cannot be empty.'})
+  @MaxLength(50, { message: '{name} must be shorter than or equal to 30 characters.'})
   name: string;
 }
