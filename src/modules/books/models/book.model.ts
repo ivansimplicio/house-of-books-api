52fe0fcc08c45dee2a06c43f28e36574dcee20df
ivansimplicio <ivansimplicio@outlook.com>
2022-07-06 10:30:56 -0300

    feat: created Book model

---
+++ src/modules/books/models/book.model.ts
@@ -0,0 +1,47 @@
+import { ObjectType, Field, ID, Float, Int } from '@nestjs/graphql';
+
+@ObjectType()
+export class Book {
+  
+  @Field(() => ID)
+  id: number;
+
+  @Field()
+  title: string;
+
+  @Field()
+  subtitle: string;
+
+  @Field()
+  description: string;
+
+  @Field()
+  isbn: string;
+
+  @Field()
+  author: string;
+
+  @Field()
+  publishingCompany: string;
+
+  @Field()
+  language: string;
+
+  @Field(() => Date)
+  releaseDate: Date;
+
+  @Field()
+  coverType: string;
+
+  @Field(() => Int)
+  numberOfPages: number;
+
+  @Field(() => Float)
+  value: number;
+
+  @Field(() => Date)
+  createdAt: Date;
+
+  @Field(() => Date)
+  updatedAt: Date;
+}
