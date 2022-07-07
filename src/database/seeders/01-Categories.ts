991feb77214ec9306636349b0cd03711cd84bec5
ivansimplicio <ivansimplicio@outlook.com>
2022-07-07 15:25:02 -0300

    feat: seeder created for Category entity

---
+++ src/database/seeders/01-Categories.ts
@@ -0,0 +1,19 @@
+import { Connection } from 'typeorm';
+import { Factory, Seeder } from 'typeorm-seeding';
+import { Category } from './../../modules/categories/entities/category.entity';
+
+export default class SeedCategories implements Seeder {
+  public async run(_factory: Factory, connection: Connection): Promise<any> {
+    await connection
+      .createQueryBuilder()
+      .insert()
+      .into(Category)
+      .values([
+        { name: 'Design e UX' },
+        { name: 'Marketing Digital' },
+        { name: 'Lógica de Programação' },
+        { name: 'Games' }
+      ])
+      .execute()
+  }
+}
