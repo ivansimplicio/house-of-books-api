242da40a7f567f4df45391ccf304659c9c589b5b
ivansimplicio <ivansimplicio@outlook.com>
2022-07-08 15:01:13 -0300

    refactor: improvements to the categories service class

--- src/modules/categories/categories.service.ts
+++ src/modules/categories/categories.service.ts
@@ -4,16 +4,18 @@ import { Not, Repository } from 'typeorm';
 import { CategoryInput } from './dto/category.input';
 import { Category } from './entities/category.entity';
 
+type Query = {
+  where: any
+}
+
 @Injectable()
 export class CategoriesService {
 
   constructor(@InjectRepository(Category) private categoriesRepository: Repository<Category>) {}
 
   async create(data: CategoryInput): Promise<Category> {
-    const category = await this.categoriesRepository.findOneBy({ name: data.name });
-    if(category){
-      throw new UnprocessableEntityException('This category is already registered.');
-    }
+    const query = { where: { name: data.name } };
+    await this.findCategoryByQuery(query);
     return this.categoriesRepository.save(data);
   }
 
@@ -31,13 +33,8 @@ export class CategoriesService {
 
   async update(id: number, data: CategoryInput): Promise<Category> {
     const category = await this.findOne(id);
-    const categoryExists = await this.categoriesRepository.find({ where: {
-      name: data.name,
-      id: Not(id)
-    }});
-    if(categoryExists.length > 0){
-      throw new UnprocessableEntityException('This category is already registered.');
-    }
+    const query = { where: { name: data.name, id: Not(id) }};
+    await this.findCategoryByQuery(query);
     return this.categoriesRepository.save({ ...category, ...data });
   }
 
@@ -45,5 +42,12 @@ export class CategoriesService {
     const category = await this.findOne(id);
     const deletedCategory = await this.categoriesRepository.remove(category);
     return deletedCategory ? true : false;
+  }
+
+  private async findCategoryByQuery(query: Query): Promise<void> {
+    const category = await this.categoriesRepository.findOne(query);
+    if(category){
+      throw new UnprocessableEntityException('This category is already registered.');
+    }
   }
 }
