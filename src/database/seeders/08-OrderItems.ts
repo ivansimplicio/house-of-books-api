2de62195505c3588a5f1ee8ed313289b0d1d792c
ivansimplicio <ivansimplicio@outlook.com>
2022-07-13 19:18:08 -0300

    feat: seeder created for OrderItem entity

---
+++ src/database/seeders/08-OrderItems.ts
@@ -0,0 +1,39 @@
+import { OrderItem } from '../../modules/order-items/entities/order-item.entity';
+import { Connection } from 'typeorm';
+import { Factory, Seeder } from 'typeorm-seeding';
+
+export default class SeedOrderItems implements Seeder {
+  public async run(_factory: Factory, connection: Connection): Promise<any> {
+    await connection
+      .createQueryBuilder()
+      .insert()
+      .into(OrderItem)
+      .values([
+        {
+          id: 1,
+          quantity: 1,
+          discount: 0.1,
+          amount: 62.91,
+          bookId: 1,
+          orderId: 1
+        },
+        {
+          id: 2,
+          quantity: 1,
+          discount: 0.1,
+          amount: 62.91,
+          bookId: 2,
+          orderId: 1
+        },
+        {
+          id: 3,
+          quantity: 2,
+          discount: 0.2,
+          amount: 79.84,
+          bookId: 3,
+          orderId: 2
+        }
+      ])
+      .execute();
+  }
+}
