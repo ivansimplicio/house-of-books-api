2c9ab4d618d35229aad2b2aa13320746038e34d0
ivansimplicio <ivansimplicio@outlook.com>
2022-07-13 11:30:37 -0300

    feat: created OrderItem model

---
+++ src/modules/order-items/models/order-item.model.ts
@@ -0,0 +1,23 @@
+import { ObjectType, Field, Int, ID, Float } from '@nestjs/graphql';
+
+@ObjectType()
+export class OrderItem {
+
+  @Field(() => ID)
+  id: number;
+
+  @Field(() => Int)
+  quantity: number;
+
+  @Field(() => Float)
+  discount: number;
+
+  @Field(() => Float)
+  amount: number;
+
+  @Field(() => Date)
+  createdAt: Date;
+
+  @Field(() => Date)
+  updatedAt: Date;
+}
